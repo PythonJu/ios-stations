5a1fab178da9b70a074d0c1ad6977c7037c1d224
PythonJu <d87496789@gmail.com>
2021-09-12 22:25:48 +0900

    test4-1

--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -10,7 +10,6 @@ class FirstViewController: UIViewController {
     var books: [Book]?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = UIColor.init(named: "main")
     }
 }
